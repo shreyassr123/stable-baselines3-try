ac345144397b4fd887278614474ab6fd1098ff0f
Shreyas S R <shreyassr123@gmail.com>
2024-06-02 17:02:31 +0530

    Delete stable_baselines3/sordq directory

--- stable_baselines3/sordq/__init__.py
+++
@@ -1,4 +0,0 @@
-from stable_baselines3.sordqn.sordqn import SORDQN
-from stable_baselines3.dqn.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-
-__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "SORDQN"]
