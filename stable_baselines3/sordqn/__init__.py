572aee9c705e9e0c5bd74f39af6cf06e817fdb6c
Shreyas S R <shreyassr123@gmail.com>
2024-06-02 19:05:17 +0530

    Add files via upload

---
+++ stable_baselines3/sordqn/__init__.py
@@ -0,0 +1,4 @@
+from stable_baselines3.sordqn.sordqn import SORDQN
+from stable_baselines3.dqn.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "SORDQN"]
